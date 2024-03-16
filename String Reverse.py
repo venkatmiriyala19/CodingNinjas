1b34157e0e38a328c089013d9f6fd507a7e1a472
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-03-16 13:53:01 +0530

    Create String Reverse.py

---
+++ String Reverse.py
@@ -0,0 +1,10 @@
+from sys import *
+from collections import *
+from math import *
+
+from typing import *
+
+def stringReverse(S: str)-> str:
+    return S[::-1]
+    # Write your Code here
+    pass
