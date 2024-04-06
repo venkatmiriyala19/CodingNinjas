fc212e40cca404755a9550ef88ad873c059e72f7
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-04-06 14:03:52 +0530

    Create  Game of Stones.py

---
+++ Game of Stones.py
@@ -0,0 +1,9 @@
+def gameOfStones(num):
+    if num!=3:
+        return "Ale"
+    return "Bob"
+
+    # Write your code here
+    # Return, who will win - 'Ale or Bob'?
+    # 'num' is number of stones.
+    pass
