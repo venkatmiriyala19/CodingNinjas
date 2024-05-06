161a9a2a574b0f8449891cdb589c0b57df0202f5
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-05-06 19:47:37 +0530

    Create  Factorial of a Number.py

---
+++ Factorial of a Number.py
@@ -0,0 +1,35 @@
+#Your code goes here
+n=int(input())
+if n<0:
+    print("Error")
+    quit()
+fact=1
+for i in range(1,n+1):
+    fact*=i
+print(fact)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
