263522133f21bfb77c98283145fb937c9a8e44b3
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-05-09 14:35:05 +0530

    Create Reverse the String.java

---
+++ Reverse the String.java
@@ -0,0 +1,10 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {	
+	public static String reverseString(String str) {
+		StringBuilder rev=new StringBuilder(str).reverse();
+		String str1=rev.toString();
+		return str1;
+		// Write your code here.
+	}
+}
