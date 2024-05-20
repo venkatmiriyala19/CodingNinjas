84ae07966fe64354672be29a4cd8b5008ab37802
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-05-20 15:12:20 +0530

    Create  Search In Rotated Sorted Array.java

---
+++ Search In Rotated Sorted Array.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Solution {
+    public static int search(ArrayList<Integer> arr, int n, int k) {
+        for(int i=0;i<n;i++){
+            if (arr.get(i)==k){
+                return i;
+            }
+        }
+        return -1;
+        // Write your code here.
+    }
+}
