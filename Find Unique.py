2fa794534eae97f7e7f54002b3e45e29db12dfc6
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-05-21 13:27:22 +0530

    Create  Find Unique.py

---
+++ Find Unique.py
@@ -0,0 +1,10 @@
+def findUnique(arr, n) :
+    freq={}
+    for i in arr:
+        if i in freq:
+            freq[i]+=1
+        else:
+            freq[i]=1
+    for i in freq:
+        if freq[i]==1:
+            return i
