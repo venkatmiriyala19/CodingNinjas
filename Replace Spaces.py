57de71420375a655a42f44b31468d5a3d3208f7f
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-07-06 19:33:34 +0530

    Create Replace Spaces.py

---
+++ Replace Spaces.py
@@ -0,0 +1,10 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def replaceSpaces(str):
+    new=str.replace(" ","@40")
+    return new
+    # Write your code here.
+    pass
