2eaff2d338a639c1ab86a5aeebc2bd22af988214
Venkat Miriyala <156288519+venkatmiriyala19@users.noreply.github.com>
2024-07-19 14:14:54 +0530

    Create  Count Of Divisible Pairs.java

---
+++ Count Of Divisible Pairs.java
@@ -0,0 +1,17 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    static int countOfDivisiblePairs(int n, int m) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            for (int j=1;j<=m;j++){
+                if ((i+j)%5==0){
+                    count++;
+                }
+            }
+        }
+        return count;
+        // Write your code here.
+    }
+
+}
